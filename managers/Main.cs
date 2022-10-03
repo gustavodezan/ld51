76087099a01a6d277d9da6126a06a614267c02dd
Nathália Pereira <np.nathaliapereira@gmail.com>
2022-10-03 13:01:18 -0300

    Make long legs enemy keep a distance from the player and then shoot for a while before moving again

--- managers/Main.cs
+++ managers/Main.cs
@@ -5,12 +5,18 @@ public class Main : Node
 {
 	[Export]
 	public PackedScene ProjectileScene;
+	[Export]
+	public PackedScene LongLegsScene;
 	
 	public override void _Ready()
 	{
 		var player = GetNode<Player>("Player");
 		var startPosition = GetNode<Position2D>("StartPosition");
 		player.Start(startPosition.Position);
+		
+				
+		var longlegs = (LongLegs)LongLegsScene.Instance();
+		AddChild(longlegs);
 	}
 
 
